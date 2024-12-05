ecfee8536b8fbfcad546453e464ac69fd794934b
박민정 <>
2024-12-05 12:38:15 +0900

    feat : main add 버튼 이동 처리

--- miniary/main/MainViewController.swift
+++ miniary/main/MainViewController.swift
@@ -14,6 +14,17 @@ class MainViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func addBtn(_ sender: Any) {
+        print ("addBtn clicked!")
+        // 스토리보드 인스턴스 가져오기
+        let storyboard = UIStoryboard(name: "Add", bundle: nil)
+        
+        // AddViewController 인스턴스 가져오기
+        if let addViewController = storyboard.instantiateViewController(withIdentifier: "AddViewController") as? AddViewController {
+            // 뷰 컨트롤러를 NavigationController로 푸시
+            self.navigationController?.pushViewController(addViewController, animated: true)
+        }
+    }
+    
 }
 
