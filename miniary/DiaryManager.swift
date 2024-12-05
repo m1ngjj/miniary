eee6a373f9b56509654b1ca6e9861186f48a37e7
박민정 <>
2024-12-05 11:48:50 +0900

    feat : DiaryManager 파일

---
+++ miniary/DiaryManager.swift
@@ -0,0 +1,28 @@
+//
+//  DiaryManager.swift
+//  miniary
+//
+//  Created by 박민정 on 12/5/24.
+//
+import UIKit
+
+struct Diary {
+    let emoji: String
+    let date: Date
+    let content: String
+    let image: UIImage
+}
+
+class DiaryManager {
+    func addDiary() {
+        
+    }
+    
+    func updateDiary() {
+        
+    }
+    
+    func deleteDiary() {
+        
+    }
+}
