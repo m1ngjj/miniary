8016847a480d44cd2bb7059a032840ce1882375c
m1ngjj <businm1n1@gmail.com>
2024-12-09 08:57:40 +0900

    feat : emojiTextField에 하나의 이모지만 입력가능하게 처리

--- miniary/add/AddViewController.swift
+++ miniary/add/AddViewController.swift
@@ -7,10 +7,14 @@
 
 import UIKit
 
-class AddViewController: UIViewController {
+class AddViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var emojiTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        emojiTextField.delegate = self // Delegate 설정
 
         setupTapGesture()
     }
@@ -23,5 +27,24 @@ class AddViewController: UIViewController {
 
     @objc func dismissKeyboard() {
         view.endEditing(true)
+    }
+    
+    // UITextFieldDelegate 메서드
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        // 이모지가 아닌 입력 차단
+        for scalar in string.unicodeScalars {
+            if !scalar.properties.isEmoji {
+                return false
+            }
+        }
+        
+        // 새 텍스트가 입력될 때 기존 텍스트를 대체
+        if let currentText = textField.text, !currentText.isEmpty {
+            textField.text = string // 새 입력으로 기존 텍스트 대체
+            return false // 기존 텍스트 대체했으므로 추가 입력 차단
+        }
+        
+        // 현재 텍스트가 비어 있으면 입력 허용
+        return true
     }
 }
